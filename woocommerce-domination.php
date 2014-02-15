643cfbb7803000b6d831488b8d5e02b614f6accd
claudiosanches <claudiosanches@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-15 00:21:51 +0000

    updated the plugin version to 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-domination/tags/1.0.0@858163 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ woocommerce-domination.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * WooCommerce Domination.
+ *
+ * @package   WooCommerce_Domination
+ * @author    Claudio Sanches <contato@claudiosmweb.com>
+ * @license   GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WooCommerce Domination
+ * Plugin URI:        https://github.com/claudiosmweb/woocommerce-domination
+ * Description:       Allows the WooCommerce take the control of your WordPress admin.
+ * Version:           1.0.0
+ * Author:            claudiosanches
+ * Author URI:        http://claudiosmweb.com/
+ * Text Domain:       woocommerce-domination
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/claudiosmweb/woocommerce-domination
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Public.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-wc-domination.php';
+add_action( 'plugins_loaded', array( 'WC_Domination', 'get_instance' ) );
+
+/**
+ * Admin.
+ */
+if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/admin/class-wc-domination-admin.php';
+	add_action( 'plugins_loaded', array( 'WC_Domination_Admin', 'get_instance' ), 999 );
+}
